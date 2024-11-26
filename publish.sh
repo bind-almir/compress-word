f673aba765ed15f1b74c731a78cc3194a9d041ed
Almir Zulic <almir@bind.ba>
2024-11-26 17:24:18 +0100

    fix build

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@ region="eu-west-1"
 docker buildx build --platform=linux/amd64 -t c-rest .
 docker tag c-rest:latest ${accountId}.dkr.ecr.${region}.amazonaws.com/c-rest:latest
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountId}.dkr.ecr.${region}.amazonaws.com
-docker build -t ${accountId}.dkr.ecr.${region}.amazonaws.com/c-rest:latest .
+docker buildx build --platform=linux/amd64 -t ${accountId}.dkr.ecr.${region}.amazonaws.com/c-rest:latest .
 aws ecr describe-repositories --repository-names c-rest --region ${region} || aws ecr create-repository --repository-name c-rest --region ${region}
 docker push ${accountId}.dkr.ecr.${region}.amazonaws.com/c-rest:latest
